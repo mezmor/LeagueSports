5baa0f170208632fd972d9f92433ac90a783bd04
Mezmor <lepswiss@gmail.com>
2013-10-27 23:19:02 -0700

    Commit db, add leagues app.

--- LeagueSports/settings.py
+++ LeagueSports/settings.py
@@ -121,9 +121,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
---
+++ leagues/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class FantasyTeam(models.Model):
+    
+class User(models.Model):
+    
+# Player, as well as their aggregate statistics
+class Player(models.Model): 
+    player_name = models.CharField(max_length=250, primary_key=True)
+    
+class PlayerGameStat(models.Model):
+    
+class Game(models.Model):
+    
