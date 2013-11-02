d158e8b4f81910198a0dc0035b3f59aa8db5be04
Mezmor <lepswiss@gmail.com>
2013-11-02 00:08:38 -0700

    Kind of condensed. Leaguefocus doesnt work

--- drafter/urls.py
+++ drafter/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 from drafter import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<league_id>\d+)$', views.league_focus, name='league_focus'),
-    url(r'^(?P<league_id>\d+)/$', views.league_focus, name='league_focus'),
+    url(r'^(?P<league_id>\d+)/?$', views.league_focus),
+    url(r'^$', views.index),
 )
