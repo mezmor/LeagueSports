de107afc37bbe5154feed69d3c9fd4964c6c9b21
Mezmor <lepswiss@gmail.com>
2013-11-02 13:28:48 -0700

    Added bootstrap and some templates, adjusted admin panel a bit

--- drafter/admin.py
+++ drafter/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from drafter.models import League
+from drafter.models import *
 from django.contrib.sites.models import Site
 
 class LeagueAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(League, LeagueAdmin)
+admin.site.register(Team, LeagueAdmin)
+admin.site.register(FantasyTeam, LeagueAdmin)
+admin.site.register(Player, LeagueAdmin)
+admin.site.register(Game, LeagueAdmin)
+admin.site.register(PlayerGameStat, LeagueAdmin)
 admin.site.unregister(Site)
