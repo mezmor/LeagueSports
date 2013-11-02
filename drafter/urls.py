6245bb0cf65438b6048902d805a80b8037444961
Mezmor <lepswiss@gmail.com>
2013-11-02 00:38:07 -0700

    URL redirects working.

--- drafter/urls.py
+++ drafter/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from drafter import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<league_id>\d+)/?$', views.league_focus),
     url(r'^$', views.index),
+    url(r'^(?P<league_id>\d+)/?$', views.league_focus),
+    url(r'^login/$', views.login)
 )
