0b243e75fbbd3e3aadf4ebfbc6ead067a189e28f
Mezmor <lepswiss@gmail.com>
2013-11-01 23:52:20 -0700

    Added prelim views. Some routing doesn't seem to work

--- LeagueSports/urls.py
+++ LeagueSports/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', 'LeagueSports.views.home', name='home'),
+    # Examples
+    # url(r'^$', 'LeagueSports.views.home', name='home'),
     # url(r'^LeagueSports/', include('LeagueSports.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +13,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'/^$', include('drafter.urls')),
 )
--- drafter/views.py
+++ drafter/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("No template yet :(")
+
+def login(request):
+    return HttpResponse("Login screen placeholder")
+
+def league_focus(request, league_id):
+    return HttpResponse("League screen placeholder for league: %s" % league_id)
