1484021a4b728ddee970543988bf9e95acc69c0c
Mezmor <lepswiss@gmail.com>
2013-11-02 19:16:05 -0700

    Fixed templating scheme, brought nav_bar to a decent preliminary state

--- LeagueSports/settings.py
+++ LeagueSports/settings.py
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "C:/Projects/LeagueSports/drafter/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -125,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'drafter',
+    'bootstrap3',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- drafter/views.py
+++ drafter/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return HttpResponse("No template yet :(")
+    return render(request, 'drafter/index.html')
 
 def login(request):
     return HttpResponse("Login screen placeholder")
