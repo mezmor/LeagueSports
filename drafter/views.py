d678b8ffda022e57dee38365bb9525759db3f5d7
Mezmor <lepswiss@gmail.com>
2013-11-03 16:17:26 -0800

    SETUP DAT TRIFOLD

--- drafter/views.py
+++ drafter/views.py
@@ -8,14 +8,11 @@ from django.template import Context, RequestContext
 def index(request):
     return render(request, 'drafter/index.html')
 
-def login(request):
-    return render(request, 'drafter/login.html')
-
 def league_focus(request, league_id):
     return HttpResponse("League screen placeholder for league: %s" % league_id)
 
-@login_required
-def account_focus(request):
-    return render_to_response('drafter/index.html', 
-                              {'is_auth' : request.user.is_authenticated()},
-                              context_instance=RequestContext(request))
+#@login_required
+#def account_focus(request):
+#    return render_to_response('drafter/index.html', 
+#                              {'is_auth' : request.user.is_authenticated()},
+#                              context_instance=RequestContext(request))
