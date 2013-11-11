28f841f6cbca7f08b0ad2be2f47d72d43a411140
Mezmor <lepswiss@gmail.com>
2013-11-10 23:13:29 -0800

    Made registration panel pretty. Doesn't actually function yet :(

--- drafter/views.py
+++ drafter/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.template import RequestContext
+
 
 
 def index(request):
-    return render(request, 'drafter/index.html')
+    # if request is POST, register the user
+    form = UserCreationForm()
+    context = { 'register_form' : form,  }
+    return render(request, 'drafter/index.html', context, context_instance=RequestContext(request))
 
 def league_focus(request, league_id):
     return HttpResponse("League screen placeholder for league: %s" % league_id)
