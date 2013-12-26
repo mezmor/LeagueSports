749c3b979db24c42501e0cda2fbff56f71598e5c
Lucas Pfister <lepswiss@gmail.com>
2013-12-26 10:15:25 -0800

    Removed async:false, made the response a valid JSON object

--- explorer/views.py
+++ explorer/views.py
@@ -10,12 +10,13 @@ def get_player_names(request):
     if request.method == 'GET':
         all_players = list(RawPlayerData.objects.values_list('player_name', flat=True))
         
-        response_data = []
+        response_data = {}
+        response_data['data'] = []
         for name in sorted(set(all_players)):
             if name != "":
                 player_data = {}
                 player_data['name'] = name
                 player_data['sample'] = all_players.count(name)
-                response_data.append(player_data)
+                response_data['data'].append(player_data)
             
         return HttpResponse(json.dumps(response_data), content_type="application/json")
