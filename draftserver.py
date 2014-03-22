b676b5f8da9b47e0f0e45d165b32b84d01a6f972
Lucas Pfister <lepswiss@gmail.com>
2014-03-21 18:05:27 -0700

    Extract sessionid from request cookie

--- draftserver.py
+++ draftserver.py
@@ -7,20 +7,30 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LeagueSports.settings")
 
 from django.contrib.sessions.models import Session
+from drafter.models import ConnectionTicket
  
 
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
+        cookie = self.request.headers.get('cookie')
+        split_cookie = cookie.split(';')
+        sessionIndex = [index for index, data in enumerate(split_cookie) if "sessionid" in data]
+        sessionid = split_cookie[sessionIndex[0]].split("=")[1]
+        print sessionid
         print 'new connection'
       
     def on_message(self, message):
-        print 'message received: %s' % message
-        if message.startswith('session'):
-            data = message.split(":")
-            print data
+        data = message.split(":")
+        print 'data received: %s' % data
+        if len(data) < 2:
+            return
+        if data[0] == 'session':
             session = Session.objects.get(session_key=data[1])
-            print session.expire_date
+            print "Session: " + str(session)
             self.write_message('Data Found!')
+        if data[0] == 'ticket':
+            ticket = ConnectionTicket.objects.get()
+            print "Ticket: " + str(ticket)
  
     def on_close(self):
         print 'connection closed'
