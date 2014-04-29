dcac2df2befe0d4b4e3d7e6d0e9768235c7a170a
Lucas Pfister <lepswiss@gmail.com>
2014-04-29 09:50:40 -0700

    Stubbed out message tests

--- drafter/tests/message.py
+++ drafter/tests/message.py
@@ -11,5 +11,21 @@ class MessageViewsTestCase(TestCase):
     def setUp(self):
         self.testUser = User.objects.create_user(username='testUser', password='test1234')
         self.testLeague = League.objects.create(name="testLeague", public=True, size=12, commish_id=self.testUser.id)
-        
     
+    """
+    Test the new join requests view
+    """
+    def test_new_requests(self):
+        pass
+    
+    """
+    Test the request creation view
+    """
+    def test_request_creation(self):
+        pass
+    
+    """
+    Test the request deletion view
+    """
+    def test_request_deletion(self):
+        pass
